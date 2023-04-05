ab8c191438c9824df125fb2577710dc19e396b73
Daehan930 <asdsad8835@gmail.com>
2023-04-05 18:20:48 +0900

    0405Main

--- src/Main.js
+++ src/Main.js
@@ -1,146 +1,169 @@
-import React, { useEffect, useState } from "react";
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import "./Main.scss";
-   
-function Main(){
-    const [channelData, setChannelData] = useState({});
 
-  const [playlistsData, setPlaylistsData] = useState([]);
-  
+const initialChannelData = {
+  name: "채널 이름",
+  logo: "로고 이미지",
+  description: "채널 소개",
+  subscriberCount: 0,
+  videoCount: 0,
+  viewCount: 0,
+};
+
+function Main() {
   const API_KEY = "AIzaSyBeRoJxa8dsDPjG789v-azHEkrbKYn_qME";
-  const channelId = "UCSLrpBAzr-ROVGHQ5EmxnUg";
 
-  
-    const [offset, setOffset] = useState(0);
-  
-    const slide = (direction) => {
-      const newOffset = offset + direction * 4;
-      if (newOffset >= 0 && newOffset < playlistsData.length) {
-        setOffset(newOffset);
-      }
-    };
-    
-    const variants = {
-      hidden: (index) => ({
-        x: index < offset ? -100 : 100,
-        opacity: 0,
-      }),
-      visible: { x: 0, opacity: 1 },
-    };
-  
-  
-  // 채널의 재생목록, 동영상 관련 호출
-  async function fetchPlaylistsData() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [channelData, setChannelData] = useState(initialChannelData);
+  const [playlistsData, setPlaylistsData] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  const variants = {
+    hidden: (index) => ({
+      x: index < offset ? -100 : 100,
+      opacity: 0,
+    }),
+    visible: { x: 0, opacity: 1 },
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault(); // 기본 동작 중단
+
     try {
-      const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}&maxResults=50&key=${API_KEY}`
+      const searchResponse = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=channel&q=${searchTerm}&key=${API_KEY}`
       );
-      const data = await response.json();
-      setPlaylistsData(data.items);
-    } catch (error) {
-      console.error("Failed to fetch playlists data:", error);
-    }
-  }
+      const searchResult = await searchResponse.json();
 
-  // 채널 기본정보 호출
-    useEffect(() => {
-      async function fetchChannelData() {
-        try {
-          const response = await fetch(
-          `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`
-          );
-          const data = await response.json();
-          const channelItem = data.items[0];
-          const snippet = channelItem.snippet;
-          const statistics = channelItem.statistics;
-
-          setChannelData({
-            logo: snippet.thumbnails.default.url,
-            name: snippet.title,
-            description: snippet.description,
-          subscriberCount: statistics.subscriberCount,
-          videoCount: statistics.videoCount,
-          viewCount: statistics.viewCount,
-          });
-        // {
-        //   console.log(statistics);
-        // }
-        } catch (error) {
-        console.error("Failed to fetch channel data:", error);
-        }
+      if (searchResult.items.length === 0) {
+        setChannelData(initialChannelData);
+        setPlaylistsData([]);
+        return;
       }
-  
-      fetchChannelData();
-    fetchPlaylistsData();
-    }, []);
 
-  function formatNumber(num) {
-    num = num || 0; // num이 undefined일 경우 기본값 0으로 설정
-    if (num >= 10000) {
-      return (num / 10000).toFixed(1) + "만";
-    } else if (num >= 1000) {
-      return (num / 1000).toFixed(1) + "천";
-    } else {
-      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //전체 조회수 3자리수마다 " , " 찍기
+      const channelId = searchResult.items[0].snippet.channelId;
+
+      const channelResponse = await fetch(
+        `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`
+      );
+      const channelResult = await channelResponse.json();
+
+      setChannelData({
+        name: channelResult.items[0].snippet.title,
+        logo: channelResult.items[0].snippet.thumbnails.high.url,
+        description: channelResult.items[0].snippet.description,
+        subscriberCount:
+          +channelResult.items[0].statistics.subscriberCount.toLocaleString(),
+        videoCount:
+          +channelResult.items[0].statistics.videoCount.toLocaleString(),
+        viewCount:
+          +channelResult.items[0].statistics.viewCount.toLocaleString(),
+      });
+
+      const playlistsResponse = await fetch(
+        `https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=12&channelId=${channelId}&key=${API_KEY}`
+      );
+      const playlistsResult = await playlistsResponse.json();
+
+      setPlaylistsData(playlistsResult.items);
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
+
+  const handleSlide = (direction) => {
+    if (direction === -1 && offset > 0) {
+      setOffset(offset - 4);
+    } else if (direction === 1 && offset < playlistsData.length - 4) {
+      setOffset(offset + 4);
+    }
+  };
 
   return (
-    <header>
-      <div className="glass">
-        <div className="channel-info">
-         <h1 className="channel-name">{channelData.name}</h1>
+    <div className="App">
+      <header>
+        <div className="glass">
 
-          <img
-            className="channel-logo"
-            src={channelData.logo}
-            alt="채널 로고"
-          />
-
-        <p className="channel-description">{channelData.description}</p>
-
-          <div className="channel-statistics">
-            <p>구독자 수: {formatNumber(channelData.subscriberCount)} 명</p>
-          <p>동영상 수: {channelData.videoCount}</p>
-            <p>전체 조회수: {formatNumber(channelData.viewCount)} 회</p>
-        </div>
-      </div>
-
-      <button className="btn-left" onClick={() => slide(-1)}>이전</button>
-
-      <div className="playlists-container">
-      <h2 className="playlists-title">재생 목록</h2>
-        <AnimatePresence>
-          {playlistsData.slice(offset, offset + 4).map((playlist, index) => (
-            <motion.div
-              className="playlist"  
-              key={playlist.id}
-              initial="hidden"
-              animate="visible"
-              exit="fadeOut" 
-              custom={index}
-              variants={variants}
-              transition={{ duration: 0.2 }}
-              >
-              <img
-                className="playlist-thumbnail"
-                src={playlist.snippet.thumbnails.high.url}
-                alt="재생 목록 썸네일"
+          <form  className="search-box" onSubmit={handleSearch}>
+            <div>
+              <input
+                type="text"
+                placeholder="채널 검색"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <p className="playlist-title">{playlist.snippet.title}</p>
-            </motion.div>
-          ))}
-        </AnimatePresence>
+              <button type="submit">검색</button>
+            </div>
+          </form>
 
+          <h1 className="channel-name">{channelData.name}</h1>
+
+          <div className="channel-info">
+            <img
+              className="channel-logo"
+              src={channelData.logo}
+              alt="채널 로고"
+            />
+
+            <div className="channel-statistics">
+              <p>
+                구독자 수: {channelData.subscriberCount.toLocaleString()} 명
+              </p>
+              <p>동영상 수: {channelData.videoCount.toLocaleString()} 개</p>
+              <p>전체 조회수: {channelData.viewCount.toLocaleString()} 회</p>
+            </div>
+          </div>
+
+          <p
+            style={{ textAlign: "center", fontSize: "1rem" }}
+            className="channel-description"
+          >
+            {channelData.description}
+          </p>
+
+          <button className="btn-left" onClick={() => handleSlide(-1)}>
+            이전
+          </button>
+
+          <div className="playlists-container">
+            <h2 className="playlists-title">재생 목록</h2>
+            {playlistsData.length > 0 ? (
+              <AnimatePresence>
+                {playlistsData
+                  .slice(offset, offset + 4)
+                  .map((playlist, index) => (
+                    <motion.div
+                      className="playlist"
+                      key={playlist.id}
+                      initial="hidden"
+                      animate="visible"
+                      exit="fadeOut"
+                      custom={index}
+                      variants={variants}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <img
+                        className="playlist-thumbnail"
+                        src={playlist.snippet.thumbnails.high.url}
+                        alt="재생 목록 썸네일"
+                      />
+                      <p className="playlist-title">{playlist.snippet.title}</p>
+                    </motion.div>
+                  ))}
+              </AnimatePresence>
+            ) : (
+              <p>검색 결과가 없습니다.</p>
+            )}
+          </div>
+
+          <button className="btn-right" onClick={() => handleSlide(1)}>
+            다음
+          </button>
+        </div>
+      </header>
     </div>
-    <button className="btn-right" onClick={() => slide(1)}>다음</button>
-
-
-
-      </div>
-    </header>
   );
 }
-
 
 export default Main;
---
+++ src/test.js
@@ -0,0 +1,143 @@
+// import React, { useEffect, useState } from "react";
+// import { motion, AnimatePresence } from "framer-motion";
+// import "./Main.scss";
+
+function Main() {
+  const [channelData, setChannelData] = useState({});
+
+  const [playlistsData, setPlaylistsData] = useState([]);
+
+  const API_KEY = "AIzaSyBeRoJxa8dsDPjG789v-azHEkrbKYn_qME";
+  const channelId = "UCSLrpBAzr-ROVGHQ5EmxnUg";
+
+  const [offset, setOffset] = useState(0);
+
+  const slide = (direction) => {
+    const newOffset = offset + direction * 4;
+    if (newOffset >= 0 && newOffset < playlistsData.length) {
+      setOffset(newOffset);
+    }
+  };
+
+  const variants = {
+    hidden: (index) => ({
+      x: index < offset ? -100 : 100,
+      opacity: 0,
+    }),
+    visible: { x: 0, opacity: 1 },
+  };
+
+  // 채널의 재생목록, 동영상 관련 호출
+  async function fetchPlaylistsData() {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}&maxResults=50&key=${API_KEY}`
+      );
+      const data = await response.json();
+      setPlaylistsData(data.items);
+    } catch (error) {
+      console.error("Failed to fetch playlists data:", error);
+    }
+  }
+
+  // 채널 기본정보 호출
+  useEffect(() => {
+    async function fetchChannelData() {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`
+        );
+        const data = await response.json();
+        const channelItem = data.items[0];
+        const snippet = channelItem.snippet;
+        const statistics = channelItem.statistics;
+
+        setChannelData({
+          logo: snippet.thumbnails.default.url,
+          name: snippet.title,
+          description: snippet.description,
+          subscriberCount: statistics.subscriberCount,
+          videoCount: statistics.videoCount,
+          viewCount: statistics.viewCount,
+        });
+
+      } catch (error) {
+        console.error("Failed to fetch channel data:", error);
+      }
+    }
+
+    fetchChannelData();
+    fetchPlaylistsData();
+  }, []);
+
+  function formatNumber(num) {
+    num = num || 0; // num이 undefined일 경우 기본값 0으로 설정
+    if (num >= 10000) {
+      return (num / 10000).toFixed(1) + "만";
+    } else if (num >= 1000) {
+      return (num / 1000).toFixed(1) + "천";
+    } else {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //전체 조회수 3자리수마다 " , " 찍기
+    }
+  }
+
+  return (
+    <header>
+      <div className="glass">
+        <div className="channel-info">
+          <h1 className="channel-name">{channelData.name}</h1>
+
+          <img
+            className="channel-logo"
+            src={channelData.logo}
+            alt="채널 로고"
+          />
+
+          <p className="channel-description">{channelData.description}</p>
+
+          <div className="channel-statistics">
+            <p>구독자 수: {formatNumber(channelData.subscriberCount)} 명</p>
+            <p>동영상 수: {channelData.videoCount}</p>
+            <p>전체 조회수: {formatNumber(channelData.viewCount)} 회</p>
+          </div>
+        </div>
+
+        <button className="btn-left" onClick={() => slide(-1)}>
+          이전
+        </button>
+
+        <div className="playlists-container">
+          <h2 className="playlists-title">재생 목록</h2>
+          <AnimatePresence>
+            {playlistsData.slice(offset, offset + 4).map((playlist, index) => (
+              <motion.div
+                className="playlist"
+                key={playlist.id}
+                initial="hidden"
+                animate="visible"
+                exit="fadeOut"
+                custom={index}
+                variants={variants}
+                transition={{ duration: 0.2 }}
+              >
+                <img
+                  className="playlist-thumbnail"
+                  src={playlist.snippet.thumbnails.high.url}
+                  alt="재생 목록 썸네일"
+                />
+                <p className="playlist-title">{playlist.snippet.title}</p>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+
+        <button className="btn-right" onClick={() => slide(1)}>
+          다음
+        </button>
+
+      </div>
+    </header>
+  );
+}
+
+// export default Main;
